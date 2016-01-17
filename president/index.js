adc37539c3f43539e81d253c92172184158ca837
Yen-chi Chen <mark86092@gmail.com>
2016-01-17 14:30:21 +0800

    新增總統資料

--- president/index.js
+++ president/index.js
@@ -2,59 +2,65 @@ var request = require('request'),
 	cheerio = require('cheerio');
 var fs = require('fs');
 
-// generate the mapping of link nodes
-var tree = require('./treeP1');
-var addArea = require('./option');
+// use the mapping
+var tree = require('../mapping/tree').root_P1;
+var goSelArea = require('../mapping/option_fix').goSelArea;
 
-tree.nodes.forEach(function(subnode) {
-	subnode.nodes.forEach(function(subsubnode){
-		addArea(subsubnode);
+tree.nodes.forEach(function(node){
+	node.nodes.forEach(function(node){
+		goSelArea(node);
 	});
 });
 
+var queue = [];
 function query(link, callback) {
-	request(link, function(error, res, body) {
-		if (!error && res.statusCode == 200) {
-			var $ = cheerio.load(body);
-			var table = $(".tableT");
-			
-			var title = [];
-			table.find(".trHeaderT td").each(function(index, element){
-				title[index] = $(this).text();
-			});
-			
-			var data = [];
-			table.find(".trT").each(function(_index, element){
-				var trData = {};
-				$(element).find("td").each(function(index, element){
-					trData[title[index]] = $(this).text();
-				});
-				data.push(trData);
-			});
-			
-			callback(data);
-		}
-	});
+	queue.push({link: link, callback: callback});
 }
 
-var count = 0;
-function doPost() {
-	count -= 1;
-	console.log(count);
-	if (count === 0) {
+function worker() {
+	var start = function() {
+		if (queue.length == 0) {
+			stop();
+			return;	
+		}
+		var job = queue.pop();
+		request({url: job.link}, function(error, res, body) {
+			if (!error && res.statusCode == 200) {
+				console.log('get ' + job.link);
+				var $ = cheerio.load(body);
+				var table = $(".tableT");
+				
+				var title = ['註記', '號次', '候選人姓名', '性別','得票數','得票率%','登記方式'];
+				var data = [];
+				table.find(".trT").each(function(_index, element){
+					var trData = {};
+					$(element).find("td").each(function(index, element){
+						trData[title[index]] = $(this).text();
+					});
+					data.push(trData);
+				});
+				
+				job.callback(data);
+			} else {
+				console.log(error);
+			}
+			setTimeout(function() {start();}, 100);
+		});
+	};
+	var stop = function() {
 		fs.writeFile('president.json', JSON.stringify(tree, null, 2), function(err) {
 			if(err) {
 				console.log(err);
 			}
 		}); 
-	}
+	};
+
+	setTimeout(function() {start();}, 100);
 }
 
 function queryNode(node) {
-	count += 1;
 	query(node.link, function(data){
 		node.data = data;
-		doPost();
 	});
 	node.nodes.forEach(function(subnode){
 		queryNode(subnode);
@@ -62,3 +68,5 @@ function queryNode(node) {
 }
 
 queryNode(tree);
+
+worker();
